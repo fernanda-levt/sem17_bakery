c0be2ef42178d95eb3e2ae0a3d793531dc27c647
fernanda-levt <mfernandalevanotello@gmail.com>
2026-01-03 12:59:54 -0500

    Add Hero

--- bakery/src/App.jsx
+++ bakery/src/App.jsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero"
+
 function App() {
   return (
     <div>
-      <h1>Bakery</h1>
+      <Hero />
     </div>
   )
 }
---
+++ bakery/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h2>LET’S TASTE PERFECTION</h2>
+        <button>ORDER NOW</button>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
